760b1010d906eddac4044fc6025133e50122dd10
draganoxfool <draganogaming1@gmail.com>
2026-07-15 01:06:29 +0500

    Bump to v1.1.1 (rebuild with updated README for PyPI)

--- easydotdict/__init__.py
+++ easydotdict/__init__.py
@@ -1,7 +1,7 @@
 from .easydotdict import dotdict
 from .decorators import dotdictify, defaults, expect_schema, freeze
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __all__ = [
     "dotdict",
     "dotdictify",
